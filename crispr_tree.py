2130906401bf9ad9ac63bc2486ab0d6f5a0a210a
liggettla <alexander.liggett@gmail.com>
2021-10-15 16:51:49 -0400

    updates

--- crispr_tree.py
+++ crispr_tree.py
@@ -17,9 +17,10 @@ def readFile(indir, filename):
 
             # sequence line
             elif count == 2:
-                gene, seq = checkMatch(line.decode("utf-8").strip())
+                line = line.decode("utf-8").strip()
+                gene, seq = checkMatch(line)
                 if gene:
-                    print('{} Match in {}:\n{}'.format(gene, filename, seq))
+                    print('{} Match in {}:\n{}'.format(gene, filename, line))
                 count += 1
 
             elif count == 3:
@@ -31,12 +32,15 @@ def readFile(indir, filename):
 
 # check if line contains AAVS1 or CHEK2 sequence
 def checkMatch(line):
+    # 8bp UMI
+    # 14 bp linker
+
     # if distance(line[10:20], aavs1[0:15]) <= 2:
-    if line[10:15] in aavs1:
+    if line[25:35] in aavs1:
         return 'AAVS1', aavs1
 
     #  if distance(line[10:20], chek2[0:15]) <= 2:
-    if line[20:25] in chek2:
+    if line[25:35] in chek2:
         return 'CHEK2', chek2
 
     else:
@@ -44,13 +48,13 @@ def checkMatch(line):
 
 # locate any CRISPR edits
 def getEdit():
+    pass
     sub = 'GGGCT'
     ctrl_start = aavs1.index(sub)
-    ctrl = (ctrl_start:ctrl_start+20)
+    ctrl = (ctrl_start,ctrl_start+20)
     test_start = line.index(sub)
-    test = (test_start:test_start+20)
+    test = (test_start,test_start+20)
     print(distance(ctrl, test))
-    pass
 
 if __name__ == '__main__':
     indir = '../test'
